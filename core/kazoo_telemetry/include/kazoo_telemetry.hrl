e37c75111596e6a834bc80cce8ae7a43fefec367
John White <john@mk1s.com>
2019-12-23 08:50:30 -0600

    expand kazoo_telemetry config macros

--- core/kazoo_telemetry/include/kazoo_telemetry.hrl
+++ core/kazoo_telemetry/include/kazoo_telemetry.hrl
@@ -9,7 +9,23 @@
 %%%-----------------------------------------------------------------------------
 -ifndef(KAZOO_TELEMETRY_HRL).
 
--define(CONFIG_TELEMETRY_CAT, <<"telemetry">>).
+-define(TELEMETRY_CAT, <<"telemetry">>).
+
+-define(TM_DEFAULT_LEADER_TICK, 60000).
+-define(TM_DEFAULT_LEADER_APP, <<"ecallmgr">>).
+-define(TM_DEFAULT_RESPONDERS, [<<"waveguide_responder">>]).
+
+-define(TM_LEADER_TICK
+       ,kapps_config:get_integer(?TELEMETRY_CAT, <<"leader_tick_ms">>, ?TM_DEFAULT_LEADER_TICK, <<"default">>)
+       ).
+
+-define(TM_LEADER_APP
+       ,kapps_config:get_ne_binary(?TELEMETRY_CAT, <<"leader_app">>, ?TM_DEFAULT_LEADER_APP, <<"default">>)
+       ).
+
+-define(TM_RESPONDERS
+       ,kapps_config:get_ne_binaries(?TELEMETRY_CAT, <<"responders">>, ?TM_DEFAULT_RESPONDERS, <<"default">>)
+       ).
 
 -define(KAZOO_TELEMETRY_HRL, 'true').
 -endif.
