62993eff1df2c85b19020dd948132c2d08fc3aa3
John White <john@mk1s.com>
2020-01-07 10:10:29 -0600

    fixup spec

--- core/kazoo_telemetry/src/extractors/kztm_services_extractor.erl
+++ core/kazoo_telemetry/src/extractors/kztm_services_extractor.erl
@@ -1,5 +1,5 @@
 %%%-----------------------------------------------------------------------------
-%%% @copyright (C) 2010-2019, 2600Hz
+%%% @copyright (C) 2010-2020, 2600Hz
 %%% @doc
 %%% This Source Code Form is subject to the terms of the Mozilla Public
 %%% License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -14,11 +14,11 @@
 -export ([extract/0]).
 
 %%------------------------------------------------------------------------------
-%% @doc
+%% @doc extract cascaded service items
 %% @end
 %%------------------------------------------------------------------------------
--spec extract() -> kz_term:proplist().
+-spec extract() -> kz_json:object().
 extract() ->
     {'ok', MasterAccountId} = kapps_util:get_master_account_id(),
     Services = kz_services:fetch(MasterAccountId),
-    [{<<"service_items">>, kz_services_quantities:fetch_cascade(Services)}].
+    kz_json:from_list([{<<"service_items">>, kz_services_quantities:fetch_cascade(Services)}]).
